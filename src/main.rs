6a30a30d2ee51fdd62d43b8348acf890cbd7e268
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-07-12 18:49:55 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::process::ExitCode;
+
+use regex::Regex;
+
+fn main() -> ExitCode {
+    let emailoid = Regex::new(r"\w+\s*\S?(@|at)\S?\s*\w+\s*\S?(\.|dot)\S?\s+com")
+        .unwrap_or_else(|_| unreachable!());
+    let space = Regex::new(r"\s").unwrap_or_else(|_| unreachable!());
+    let atoid = Regex::new(r"\S?(@|at)\S?").unwrap_or_else(|_| unreachable!());
+
+    let mut stat = ExitCode::SUCCESS;
+
+    for f_cont in std::env::args()
+        // ignore program name
+        .skip(1)
+        // files should be small,
+        // otherwise blame the user
+        .map(std::fs::read_to_string)
+        .filter_map(|f| match f {
+            Ok(s) => Some(s),
+            // warn on error
+            Err(e) => {
+                stat = ExitCode::FAILURE;
+                eprintln!("{e}");
+                None
+            }
+        })
+    {
+        // find all sub-strs that look like email addresses
+        for m in emailoid.find_iter(&f_cont) {
+            let no_ws = space.replace_all(m.as_str(), "");
+            let email = atoid.replace_all(&no_ws, "@");
+            println!("{email}");
+        }
+    }
+    stat
+}
