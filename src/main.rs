afef758e2a4f52e81013e6befbd7fc8b7bbbdf64
Rudxain <rudxain@localhost.localdomain>
2024-07-12 20:39:05 -0400

    begin prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let re = Regex::new(r"(?-u:\w+\s*.?(at|@).?\s*\w+\s*.?(\.|dot).?\s+com)")
+        .unwrap_or_else(|_| unreachable!());
+
+    for file in std::env::args()
+        // ignore program name
+        .skip(1)
+        // files should be small,
+        // otherwise we blame the user
+        .map(std::fs::read_to_string)
+        // ignore errors
+        .filter_map(Result::ok)
+    {}
 }
