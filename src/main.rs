b2ccaef14bbb5bd40e94d96cb46d1e7523cb1695
Rudxain <rudxain@localhost.localdomain>
2024-07-12 23:10:49 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use regex::Regex;
 
 fn main() {
-    let re = Regex::new(r"(?-u:\w+\s*.?(at|@).?\s*\w+\s*.?(\.|dot).?\s+com)")
+    let emailoid = Regex::new(r"\w+\s*\S?(@|at)\S?\s*\w+\s*\S?(\.|dot)\S?\s+com")
         .unwrap_or_else(|_| unreachable!());
+    let space = Regex::new(r"\s").unwrap_or_else(|_| unreachable!());
 
-    for file in std::env::args()
+    std::env::args()
         // ignore program name
         .skip(1)
         // files should be small,
-        // otherwise we blame the user
+        // otherwise blame the user
         .map(std::fs::read_to_string)
-        // ignore errors
-        .filter_map(Result::ok)
-    {}
+        .filter_map(|f| match f {
+            Ok(s) => Some(s),
+            // warn on error
+            Err(e) => {
+                eprintln!("{e}");
+                None
+            }
+        })
+        .map(|s| {
+            emailoid
+                .find_iter(&s)
+                .map(|m| space.replace(m.as_str(), ""))
+        });
 }
